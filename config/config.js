c0c8d59199235fba6d29cd1a96be8225377d6065
mhsy <hasyim11c@gmail.com>
2023-12-26 23:20:57 +0700

    build: add env package and configure it

--- config/config.js
+++ config/config.js
@@ -1,30 +1,33 @@
+require('dotenv').config()
+
 const {
-  DB_USERNAME = "BD_USERNAME",
-  DB_PASSWORD = "DB_PASSWORD",
-  DB_NAME = "DB_NAME",
-  DB_HOST = "localhost"
-} = process.env;
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST
+} = process.env
 
 module.exports = {
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "postgres"
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: 'postgres'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres'
   }
 }
